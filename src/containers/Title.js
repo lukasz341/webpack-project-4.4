2d78e9986ba6499a5d1a6a340767c92faf7b64b4
lukasz341 <lukasz341@tlen.pl>
2017-12-11 23:36:51 +0100

    webpack

---
+++ src/containers/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Title extends React.Component {
+
+
+    render() {
+        return (
+            <p>
+                Tytuł aplikacji
+            </p>
+        );
+    }
+}
+
+export default Title;
